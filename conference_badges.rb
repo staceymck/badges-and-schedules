944c5134435e9cc8a146dd0f69a575d8d625c414
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-10 10:28:23 -0600

    commit tree 80d61de7901a99ab84bd74e576654052c6c56035

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.map do |name|
+        badge_maker(name)
+    end
+end
+
+def assign_rooms(attendees)
+    attendees.map.with_index(1) do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer(attendees)
+    badges = batch_badge_creator(attendees)
+    room_assignments = assign_rooms(attendees)
+    
+    badges.each {|badge| puts badge}
+    room_assignments.each {|assignment| puts assignment}
+end
